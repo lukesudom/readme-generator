a209ae9378568d1fbee9f91ef65c56a67ed9d776
Lukesudom <lukemarshall511@gmail.com>
2022-07-12 22:43:27 +1000

    added markdown and write readme functionality

---
+++ README.md
@@ -0,0 +1,30 @@
+
+        # Luke
+
+        ## Table of Content
+        - [Project description] (#Description)
+        - [Usage] (#Usage)
+        - [Contributing] (#Contributiing)
+        - [Installation] (#Installation)
+        - [Questions] (#Questions)
+        - [License] (#License)
+
+        ## Description
+        test
+
+        ## Usage
+        personal
+
+        ## Contributing
+        undefined
+
+        ## Installation
+        install
+
+        ## Questions
+        
+        
+
+        ## License
+        gnuplv3
+        
--- index.js
+++ index.js
@@ -1,3 +1,97 @@
-import inquirer from 'inquirer';
+import inquirer from 'inquirer'
 
-console.log('readme gen running');
+import fs from 'fs'
+
+import MarkDown from './lib/ReadmeGen.js'
+
+
+// app questions 
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project Title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project Description?',
+    },
+
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation instructions?',
+    },
+
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Project Usage?',
+    },
+
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Contribution info?',
+    },
+
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Tests?',
+    },
+
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Contribution info?',
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'For questions (email)?',
+    },
+
+    {
+        type: 'input',
+        name: 'github',
+        message: 'For questions (github)?',
+    },
+
+    {
+        type: 'list',
+        name: 'license',
+        message: 'License?',
+        choices: ['MIT','ISC','GNUPlv3'],
+        filter(val) {
+            return val.toLowerCase();
+        }
+    },
+]
+
+
+// run query function
+
+async function runQuery() {
+    return inquirer.prompt(questions)
+    .then((answers) => {
+        const mark = MarkDown.generateReadme(answers)
+        fs.writeFile('README.md', mark, function(err) {
+            if(err){
+                console.log('File could not be saved')
+            } else {
+                console.log('SUCCESS new README file created')
+            }
+
+        })
+       
+        
+    })
+    .catch ((error) => {
+        console.log(error)
+    })
+}
+
+runQuery()
